1ea67a9362a140cb0062f4b2166bc25e5e0f7256
Tyler Christenson <tchrist7@uoregon.edu>
2021-04-16 14:30:40 -0700

    Updated double jump
    No longer increases horizontal speed in the air.  IsGrounded check is now more refined and consistent.

--- Rollaball/Assets/Scripts/PlayerController.cs
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -5,67 +5,93 @@ using UnityEngine.InputSystem;
 using TMPro;
 
 public class PlayerController : MonoBehaviour {
-    public float speed = 0;
-    public float jumpForce = 20f;
+    public float speed;
+    public float jumpSpeed;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
     private Rigidbody rb;
+    private Collider collider;
+    // private Transform transform;
     private int count;
     private float movementX;
-    private float movementY;
-    private bool isGrounded;
+    private float movementZ;
+    // private bool isGrounded;
     private bool canDoubleJump;
+    private float distToGround;
+    
 
     // Start is called before the first frame update
-    void Start() {
+    void Start() 
+    {
         rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
+        // transform = GetComponent<Transform>();
         count = 0;
 
         SetCountText();
         winTextObject.SetActive(false);
+
+        distToGround = collider.bounds.extents.y;
     }
 
-    public void Move(InputAction.CallbackContext context) {
+    public void Move(InputAction.CallbackContext context) 
+    {
         Vector2 movementVector = context.ReadValue<Vector2>();
         movementX = movementVector.x;
-        movementY = movementVector.y;
+        movementZ = movementVector.y;
     }
 
-    public void Jump(InputAction.CallbackContext context) {
-        if (context.performed) {
-            if (isGrounded) {
-                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            if (IsGrounded()) 
+            {
+                rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
                 canDoubleJump = true;
             } else {
-                if (canDoubleJump) {
-                canDoubleJump = false;
-                rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+                if (canDoubleJump)
+                {
+                    rb.velocity = new Vector3(rb.velocity.x, jumpSpeed, rb.velocity.z);
+                    canDoubleJump = false;
                 }
             }
         }
     }
 
-    void SetCountText() {
+    void SetCountText() 
+    {
         countText.text = "Count: " + count.ToString();
-        if (count >= 12) {
+        if (count >= 12) 
+        {
             winTextObject.SetActive(true);
         }
     }
 
-    void FixedUpdate() {
-        isGrounded = (rb.position.y == 0.5f);
-
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement * speed);
+    void FixedUpdate() 
+    {
+        // IsGrounded() = (0.4999f <= rb.position.y && rb.position.y <= 0.5001f);
+        if (IsGrounded())
+        {
+            Vector3 movement = new Vector3(movementX, 0f, movementZ);
+            rb.AddForce(movement * speed);
+        }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("PickUp")) {
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
             other.gameObject.SetActive(false);
             count++;
 
             SetCountText();
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
     }
 }
